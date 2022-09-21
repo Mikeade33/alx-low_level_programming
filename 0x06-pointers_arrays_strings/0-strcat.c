0aa9ddbd186b7b6adac55a906d5d50c1c5b05e83
Michael Adebowale <mikeade33@gmail.com>
2022-09-21 10:47:28 -0700

    Commiting progress made on project

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * _strcat - concatenate two strings
- *   * @dest: char string to concatenate to
- *    * @src: char string
- *     * Return: pointer to resulting string `dest`
- *      */
+ * _strcat - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string `dest`
+ */
 
 char *_strcat(char *dest, char *src)
 {
