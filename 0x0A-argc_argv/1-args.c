80b494fc5b4731f228b0ef56df48028517ad98bf
Michael Adebowale <mikeade33@gmail.com>
2022-09-30 12:32:42 -0700

    Task 2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 /**
+ * main - Prints the number of arguments passed to it.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
  * Return: Always 0.
  */
 
